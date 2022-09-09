1975be04e4fa93be6349c58fea3147d1c30e456d
Oshane Thomas <othomas2@illinois.edu>
2022-09-08 22:42:39 -0500

    Add files via upload

---
+++ SURFMNet/__init__.py
@@ -0,0 +1,9 @@
+from .laplace_beltrami import S_info
+from .utils import SURFMNetLoss, FunctionalMapNet, SoftcorNet
+
+__all__ = [
+    'S_info',
+    'SURFMNetLoss',
+    'FunctionalMapNet',
+    'SoftcorNet'
+]
