8405108ec29454bdd49ba342b2b9e0385b6b61b2
Oshane Thomas <othomas2@illinois.edu>
2022-09-08 22:49:01 -0500

    Delete SURFMNet directory

--- SURFMNet/__init__.py
+++
@@ -1,9 +0,0 @@
-from .laplace_beltrami import S_info
-from .utils import SURFMNetLoss, FunctionalMapNet, SoftcorNet
-
-__all__ = [
-    'S_info',
-    'SURFMNetLoss',
-    'FunctionalMapNet',
-    'SoftcorNet'
-]
